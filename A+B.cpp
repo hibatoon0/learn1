8b823b0f10b016816ee73147709d32936e7437cf
hibatoon0 <hibataiebi48@gmail.com>
2026-08-15 22:13:29 +0100

    A+B MOI problem selection test

---
+++ A+B.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int N;
+    cin >> N;
+    for (int i=0; i < N; i++){
+        long long a,b;
+        cin >> a >> b;
+        cout << a + b << endl;
+        N--;
+    }
+    return 0;
+}
